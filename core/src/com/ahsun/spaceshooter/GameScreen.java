6541104d50bd4b3f6f0db3de8d3f9468b2054544
Ahsun Tariq <ahsun.tariq@hotmail.com>
2022-02-01 22:40:54 -0800

    at add player and enemy ship textures using texture pack

--- core/src/com/ahsun/spaceshooter/GameScreen.java
+++ core/src/com/ahsun/spaceshooter/GameScreen.java
@@ -5,8 +5,12 @@ import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.utils.viewport.StretchViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+
+import org.w3c.dom.Text;
 
 public class GameScreen implements Screen {
 
@@ -15,25 +19,64 @@ public class GameScreen implements Screen {
     private Viewport viewport;
 
 
+
+
     //graphics
     private SpriteBatch batch;
-    private Texture background;
+    private TextureAtlas textureAtlas;
+
+    private TextureRegion[] backgrounds;
+    private float backgroundHeight;
+
+    private TextureRegion playerShipTextureRegion, playerShieldTextureRegion, enemyShipTextureRegion, enemyShieldTextureRegion,
+    playerLaserTextureRegion, enemyLaserTextureRegion;
+
+
+
 
     //timing
-    private int backgroundOffset;
+    private float[] backgroundOffsets = {0,0,0,0};
+    private float backgroundMaxScrollingSpeed;
 
     //world parameters;
     private final int WORLD_WIDTH = 72;
     private final int WORLD_HEIGHT = 128;
 
+    //game objects
+    private Ship playerShip;
+    private Ship enemyShip;
 
     GameScreen(){
+
         camera = new OrthographicCamera();
         viewport  = new StretchViewport(WORLD_WIDTH,WORLD_HEIGHT,camera);
 
-        background = new Texture("darkPurpleStarscape.png");
-        backgroundOffset = 0;
+        //set up texture atlas
+        textureAtlas = new TextureAtlas("images.atlas");
 
+        backgrounds =  new TextureRegion[4];
+        backgrounds[0] = textureAtlas.findRegion("Starscape00");
+        backgrounds[1] = textureAtlas.findRegion("Starscape01");
+        backgrounds[2] = textureAtlas.findRegion("Starscape02");
+        backgrounds[3] = textureAtlas.findRegion("Starscape03");
+
+        backgroundHeight = WORLD_HEIGHT * 2;
+        backgroundMaxScrollingSpeed = (float) WORLD_HEIGHT/4;
+
+        //initialize texture regions
+        playerShipTextureRegion = textureAtlas.findRegion("playerShip2_blue");
+        enemyShipTextureRegion = textureAtlas.findRegion("enemyRed3");
+        playerShieldTextureRegion = textureAtlas.findRegion("shield2");
+        enemyShieldTextureRegion = textureAtlas.findRegion("shield1");
+        enemyShieldTextureRegion.flip(false,true);
+        playerLaserTextureRegion = textureAtlas.findRegion("laserBlue03");
+        enemyLaserTextureRegion = textureAtlas.findRegion("laserRed03");
+
+
+
+        //Set up game objects
+        playerShip = new Ship(2,3, 10,10, WORLD_WIDTH/2,WORLD_HEIGHT/4, playerShipTextureRegion,playerShieldTextureRegion);
+        enemyShip = new Ship(2,1, 10,10, WORLD_WIDTH/2,WORLD_HEIGHT*3/4, enemyShipTextureRegion,enemyShieldTextureRegion);
         batch = new SpriteBatch();
 
     }
@@ -44,12 +87,16 @@ public class GameScreen implements Screen {
         batch.begin();
 
         //scrolling background
-        backgroundOffset++;
-        if (backgroundOffset % WORLD_HEIGHT == 0){
-            backgroundOffset = 0;
-        }
-        batch.draw(background,0,-backgroundOffset,WORLD_WIDTH,WORLD_HEIGHT);
-        batch.draw(background,0,-backgroundOffset+WORLD_HEIGHT,WORLD_WIDTH,WORLD_HEIGHT);
+        renderBackground(deltaTime);
+
+
+        //enemy ships
+        enemyShip.draw(batch);
+        //player ship
+        playerShip.draw(batch);
+        //lasers
+
+        //explosions
         batch.end();
     }
 
@@ -85,4 +132,20 @@ public class GameScreen implements Screen {
 
     }
 
+    private void renderBackground(float deltaTime){
+
+        backgroundOffsets[0] += deltaTime*backgroundMaxScrollingSpeed/8;
+        backgroundOffsets[1] += deltaTime*backgroundMaxScrollingSpeed/4;
+        backgroundOffsets[2] += deltaTime*backgroundMaxScrollingSpeed/2;
+        backgroundOffsets[3] += deltaTime*backgroundMaxScrollingSpeed;
+
+        for (int layer = 0; layer<backgroundOffsets.length; layer++){
+            if(backgroundOffsets[layer]>WORLD_HEIGHT){
+                backgroundOffsets[layer] = 0;
+            }
+            batch.draw(backgrounds[layer],0,-backgroundOffsets[layer],WORLD_WIDTH,WORLD_HEIGHT);
+            batch.draw(backgrounds[layer],0,-backgroundOffsets[layer]+WORLD_HEIGHT,WORLD_WIDTH,WORLD_HEIGHT);
+        }
+
+    }
 }
